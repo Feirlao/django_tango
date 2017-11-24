19fd438272224d62dbcac9331cdf57dc2a50c918
lao <laorenfei@189.cn>
2017-11-24 16:55:57 +0800

    add page

--- populate_rango.py
+++ populate_rango.py
@@ -25,9 +25,9 @@ def populate():
         {"title": "part three", "url": "http://the.djangothirdparit.org/","views":32},
     ]
 
-    cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
-            "Django": {"pages": django_pages, "views": 64, "likes": 32},
-            "Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16}}
+    cats = {"python": {"pages": python_pages, "views": 128, "likes": 64},
+            "django": {"pages": django_pages, "views": 64, "likes": 32},
+            "other Frameworks": {"pages": other_pages, "views": 32, "likes": 16}}
     for cat,cat_data in cats.items():
         c=add_cat(cat,cat_data["views"],cat_data["likes"])
         for p in cat_data["pages"]:
--- rango/forms.py
+++ rango/forms.py
@@ -7,8 +7,6 @@ class CategoryForm(forms.ModelForm):
     views=forms.IntegerField(widget=forms.HiddenInput(),initial=0)
     likes= forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
-
-
     class Meta:
         model=Category
         fields=('name',)
@@ -22,3 +20,13 @@ class PageFarm(forms.ModelForm):
         model=Page
 
         exclude=('category',)
+
+    def clean(self):
+        cleaned_data=self.cleaned_data
+        url=cleaned_data.get('url')
+
+        if url and not url.startswith('https://'):
+            url='https://'+url
+            cleaned_data['url']=url
+            return cleaned_data
+
